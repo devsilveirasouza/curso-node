531d2fdc6042c321bb3f8347b718fb28cf9df7c4
devsilveirasouza <dev.silveirasouza@gmail.com>
2023-07-17 13:02:12 -0300

    Importando arquivos terceiros

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const validator_1 = __importDefault(require("validator"));
 const Matematica_1 = __importDefault(require("./Matematica")); // IMPORTANDO TUDO
 // import * as Funcoes from './funcoes';// IMPORTANDO TUDO
 // import { somar, multiplicar } from './funcoes';// IMPORTANTO COISAS ESPECIFICAS
@@ -11,3 +12,6 @@ let n2 = 2;
 console.log(`SOMA:          ${Matematica_1.default.somar(n1, n2)}`);
 console.log(`SUBTRAÇÃO:     ${Matematica_1.default.subtrair(n1, n2)}`);
 console.log(`MULTIPLICAÇÃO: ${Matematica_1.default.multiplicar(n1, n2)}`);
+console.log(validator_1.default.isEmail('suporte@gmail.com'));
+let ip = '197.0.0.2';
+console.log(validator_1.default.isIP(ip));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import validator from 'validator';
 import Funcoes from './Matematica';// IMPORTANDO TUDO
 // import * as Funcoes from './funcoes';// IMPORTANDO TUDO
 // import { somar, multiplicar } from './funcoes';// IMPORTANTO COISAS ESPECIFICAS
@@ -8,3 +9,9 @@ let n2: number = 2;
 console.log(`SOMA:          ${Funcoes.somar(n1, n2)}`);
 console.log(`SUBTRAÇÃO:     ${Funcoes.subtrair(n1, n2)}`);
 console.log(`MULTIPLICAÇÃO: ${Funcoes.multiplicar(n1, n2)}`);
+
+console.log( validator.isEmail('suporte@gmail.com') );
+
+let ip = '197.0.0.2';
+
+console.log( validator.isIP(ip) );
